102bc34a933368dc5e7cbb6e08157de6f7e2c2a2
Cindy <cindymei.huynh@gmail.com>
2019-03-29 11:31:26 -0700

    added some comments

--- bathroom_break/js/custom.js
+++ bathroom_break/js/custom.js
@@ -1,5 +1,15 @@
+//using a google maps api to display bathrooms around
+//sf market st area, users are able to add and contribute
+//to the croudsource data of bathrooms and mark whether
+//is it clean or not, public or not. using google sheets
+//as a database and ajax to retreive and send data. 
+//markers show up on the google api displaying the bathrooms
+//info box when clicked on the bathroom shows the name of 
+//bathroom. 
 var bathRoomData;
 
+//this is reading the data from the google sheet and 
+//extracting it into a usable array for my code
 function readDataAndAppend(data){
     var rows = [];
     var cells = data.feed.entry;
@@ -22,7 +32,8 @@ function readDataAndAppend(data){
 
 
 
-
+//using ajx to grab data from my google sheets source, and then 
+//calling above function and extract data to be able to use
 $(document).ready(function() {
 
 	  $.ajax({
@@ -32,13 +43,14 @@ $(document).ready(function() {
 	  	}
 	  })	
   
-  	//sticking data into the google spreadsheet
+//user able to adding data into the google sheets through our "add bathroom" form
 	$("#bathroom-form").submit(function(event){
 		event.preventDefault();
 		var data = $(this).serialize();
 		var dataArray = $(this).serializeArray();
 		console.log(dataArray)
 	
+	//using ajax to send the data into the google sheets
 		$.ajax({
 			url: "https://script.google.com/macros/s/AKfycbwfVi8Ye0M41iotzH9HikJ_fY6_aUnjTH2c8nUCfFmTUO3Vtvkr/exec",
 			type: "POST", 
@@ -47,9 +59,11 @@ $(document).ready(function() {
 				console.log("success!")
 		})
 
+    //adding the marker onto the map to be shown
 		addMarkerToMap(dataArray);
 	});
 
+//initializing the google maps api to center around sf market st
 	var mapOptions = {
 		    zoom: 16,
 		    center: {lat: 37.789, lng: -122.399}
@@ -58,6 +72,8 @@ $(document).ready(function() {
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 
+//different functions for when each button is pressed
+//to display the correct markers
   $(".button1").click(function() {
   	console.log("button1 clicked");
   	setMapOnAll(null);
@@ -84,6 +100,7 @@ $(document).ready(function() {
 });
 
 
+//displaying all the markers, dirty and clean
 var setMapOnAll= function (map) {
     for (var i = 0; i < markers.length; i++) {
       markers[i].setMap(map);
@@ -105,17 +122,23 @@ var setMarkers = function(color){
 			// make a marker
 			// make an info window
 			var color_status;
+			
+			//if the extracted data has cleaniless tag of "clean", 
+			//set the marker color to a green color
 			if (bathRoomData[i].cleanliness == "Clean") {
 				color_status = "7CFC00";
+			//else set the marker color to a red color
 			} else if (bathRoomData[i].cleanliness == "Dirty") {
 				color_status = "FF0000";
 			}
 
 
+      //creating the pin(marker) with specified color and size
 			var pin = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + color_status,
         	new google.maps.Size(21, 34));
 
 
+      //adding the marker onto the map
 			var marker = new google.maps.Marker({
 			    position: {lat: parseFloat(bathRoomData[i].lat), lng: parseFloat(bathRoomData[i].lng)},
 			    title: bathRoomData[i].company,
@@ -124,15 +147,18 @@ var setMarkers = function(color){
 		  	});
 		  	console.log(marker);
 
+      //creating an info window to display the company name of bathroom
 			var infowindow = new google.maps.InfoWindow({
 	    		content: bathRoomData[i].status + " " + bathRoomData[i].company
 		  	});
 
+        //when the marker is clicked, display the info window
 		  	google.maps.event.addListener(marker, 'click', function() {
 	   			// infowindow.open(map, marker);
 	   			infowindow.setContent( this.title );
 	   			infowindow.open( map, this );
 	  		});
+	  		//ading each marker into an array of all markers
 	  		markers.push(marker)
 
 
@@ -180,4 +206,3 @@ function addMarkerToMap(dataArray) {
 
 
 var map;
-
